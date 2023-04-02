2fbe546775ee9ffce5252f2929931b2fb234b30a
seygi.kutani <seygi@outlook.com>
2023-04-02 20:23:32 -0300

    - block to authorize routes

--- src/main/java/com/example/springbootswagger/configuration/SecurityConfig.java
+++ src/main/java/com/example/springbootswagger/configuration/SecurityConfig.java
@@ -1,34 +1,35 @@
-//package com.example.springbootswagger.configuration;
-//
-//import com.example.springbootswagger.component.KeycloakLogoutHandler;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.session.SessionRegistryImpl;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-//import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-//import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-//
-//
-//@Configuration
-//@EnableWebSecurity
-//class SecurityConfig {
-//
-//    private final KeycloakLogoutHandler keycloakLogoutHandler;
-//
-//    SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-//        this.keycloakLogoutHandler = keycloakLogoutHandler;
-//    }
-//
-//    @Bean
-//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-//        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+package com.example.springbootswagger.configuration;
+
+import com.example.springbootswagger.component.KeycloakLogoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.authorizeRequests()
 //                .requestMatchers("/api/v1/login", "/logout").permitAll()
 ////                .requestMatchers("/customers/**").hasRole("USER")
@@ -40,6 +41,26 @@
 //                .addLogoutHandler(keycloakLogoutHandler)
 //                .logoutSuccessUrl("/");
 //        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-//        return http.build();
-//    }
-//}
+
+//        http.authorizeRequests()
+//                .requestMatchers("/customers/**").hasRole("USERNEW")//.authenticated()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        http
+                .securityMatcher("/api/v1/consumers")
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().hasAnyRole("USER")//.authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/api/v1/login", "/logout");
+    }
+
+}
